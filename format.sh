ca4a6efa0c6fa44f91a87937e063e6901e1883e6
Srijan Choudhary <srijan4@gmail.com>
2014-04-27 14:11:04 +0530

    Initial Commit

---
+++ format.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+EVAL="
+(progn
+	(setq load-path
+		(cons \"erlang_mode\" load-path))
+    (require 'erlang-start)
+    (erlang-mode)
+"
+
+if [[ -f $1 ]]; then
+	EVAL+="
+	(find-file \"$1\")
+    (untabify (point-min) (point-max))
+    (delete-trailing-whitespace)
+    (erlang-indent-current-buffer)
+    (if (buffer-modified-p)
+    	(save-buffer))
+	(kill-buffer)
+	"
+elif [[ -d $1 ]]; then
+	for f in "$1"/*.erl; do
+		EVAL+="
+		(find-file \"$f\")
+	    (untabify (point-min) (point-max))
+	    (delete-trailing-whitespace)
+	    (erlang-indent-current-buffer)
+	    (if (buffer-modified-p)
+	    	(save-buffer))
+		(kill-buffer)
+		"
+	done
+else
+	echo "Usage: $0 [ <file> | <folder> ]"
+	exit
+fi
+
+EVAL+="
+    (kill-emacs))
+"
+
+emacs -q --batch --eval "${EVAL}"
